970d7cc7c28f3545a7226bcad8773c3e08cc67f6
monta <montasassi52@gmail.com>
2021-12-06 03:11:32 +0100

    firstreact

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Header from './components/Header/Header';
+import First  from './components/First/first';
+import Footer from './components/Footer/footer';
+
+function App() {
+  return (
+    <div>
+       <Header/>
+       <First/>
+       <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css";
+
+const footer = () => {
+    return (
+        <div>
+            <h3>Copyright_2021</h3>
+        </div>
+    )
+}
+
+export default footer
